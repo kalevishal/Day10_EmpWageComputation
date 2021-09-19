0f08c51233f62fa469b3760906e7ec9f6b6008d5
kalevishal <kalevishal1610@gmail.com>
2021-09-19 21:21:18 +0530

    Added Part Time employee

--- src/EmpWageBuilder.java
+++ src/EmpWageBuilder.java
@@ -1,14 +1,16 @@
 public class EmpWageBuilder {
 
     public void employeeWage() {
-        System.out.println("Welcome to Employee Wage Builder Program!!");
         int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 2;
         int EMP_RATE_PER_HR = 20;
         int empHrs = 0;
         int empWage = 0;
-        double empCheck = (Math.floor(Math.random() * 10) % 2);
+        double empCheck = (Math.floor(Math.random() * 10) % 3);
         if (empCheck == IS_FULL_TIME)
             empHrs = 8;
+        else if (empCheck == IS_PART_TIME)
+            empHrs = 4;
         else
             empHrs = 0;
 
@@ -17,7 +19,7 @@ public class EmpWageBuilder {
     }
 
     public static void main(String[] args) {
-    	EmpWageBuilder emp = new EmpWageBuilder();
+        EmpWageBuilder emp = new EmpWageBuilder();
         emp.employeeWage();
     }
 }
