b613b79b78007237e26e3656179a2a1306a505c8
kalevishal <kalevishal1610@gmail.com>
2021-09-19 21:27:53 +0530

    Modified: Monthly Wage

--- src/EmpWageBuilder.java
+++ src/EmpWageBuilder.java
@@ -1,24 +1,27 @@
 public class EmpWageBuilder {
+    int IS_FULL_TIME = 1;
+    int IS_PART_TIME = 2;
+    int EMP_RATE_PER_HR = 20;
+    int NUM_OF_WORKING_DAYS = 30;
 
     public void employeeWage() {
-        int IS_FULL_TIME = 1;
-        int IS_PART_TIME = 2;
-        int EMP_RATE_PER_HR = 20;
         int empHrs = 0;
         int empWage = 0;
-        double empCheck = (Math.floor(Math.random() * 10) % 3);
-        switch ((int) empCheck) {
-            case 1:
+        int totalEmpWage = 0;
+        for (int days = 1; days <= NUM_OF_WORKING_DAYS; days++) {
+            double empCheck = (Math.floor(Math.random() * 10) % 3);
+            if (empCheck == IS_FULL_TIME)
                 empHrs = 8;
-                break;
-            case 2:
+            else if (empCheck == IS_PART_TIME)
                 empHrs = 4;
-                break;
-            default:
+            else
                 empHrs = 0;
+
+            empWage = empHrs * EMP_RATE_PER_HR;
+            totalEmpWage += empWage;
+            System.out.println("Emp Wage is " + empWage);
         }
-        empWage = empHrs * EMP_RATE_PER_HR;
-        System.out.println("Emp Wage is " + empWage);
+        System.out.println("Total Emp Wage is " + totalEmpWage);
     }
 
     public static void main(String[] args) {
