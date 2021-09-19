bea5bc62a918271f79b7db389a43cffe162a874e
kalevishal <kalevishal1610@gmail.com>
2021-09-19 21:32:09 +0530

    Modified: Total working hrs 100 or max days 20 for month

--- src/EmpWageBuilder.java
+++ src/EmpWageBuilder.java
@@ -1,14 +1,15 @@
 public class EmpWageBuilder {
-    int IS_FULL_TIME = 1;
-    int IS_PART_TIME = 2;
-    int EMP_RATE_PER_HR = 20;
-    int NUM_OF_WORKING_DAYS = 30;
-
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+    public static final int EMP_RATE_PER_HR = 20;
+    public static final int NUM_OF_WORKING_DAYS = 30;
+    public static final int MAX_HRS_IN_MONTH=10;
     public void employeeWage() {
         int empHrs = 0;
-        int empWage = 0;
-        int totalEmpWage = 0;
-        for (int days = 1; days <= NUM_OF_WORKING_DAYS; days++) {
+        int totalEmpHrs=0;
+        int totalWorkingHrs = 0;
+        while (totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingHrs<NUM_OF_WORKING_DAYS) {
+            totalWorkingHrs++;
             double empCheck = (Math.floor(Math.random() * 10) % 3);
             if (empCheck == IS_FULL_TIME)
                 empHrs = 8;
@@ -17,10 +18,10 @@ public class EmpWageBuilder {
             else
                 empHrs = 0;
 
-            empWage = empHrs * EMP_RATE_PER_HR;
-            totalEmpWage += empWage;
-            System.out.println("Emp Wage is " + empWage);
+            totalEmpHrs+=empHrs;
+            System.out.println("Day: "+totalWorkingHrs+"Emp Hr:" + empHrs);
         }
+        int totalEmpWage=totalEmpHrs*EMP_RATE_PER_HR;
         System.out.println("Total Emp Wage is " + totalEmpWage);
     }
 
